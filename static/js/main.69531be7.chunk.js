261166f3e14f7563e93f811d9bb627bcb2c703c8
Xgrai <80608273+dxfi@users.noreply.github.com>
2023-10-24 09:52:59 +0700

    Update main.69531be7.chunk.js

--- static/js/main.69531be7.chunk.js
+++ static/js/main.69531be7.chunk.js
@@ -696,7 +696,7 @@
                                         children: Object(h.jsxs)(u.a, {
                                             width: "100%",
                                             height: "100%",
-                                            boxShadow: "#ffffff 0px 0px 10px 1px",
+                                            boxShadow: "#487748 0px 0px 10px 1px",
                                             style: {
                                                 background: "#90ee90"
                                             },
@@ -719,7 +719,7 @@
                                                 mt: 2,
                                                 mb: 2,
                                                 style: {
-                                                    boxShadow: "#ffffff 0px 0px 8px 1px",
+                                                    boxShadow: "#487748 0px 0px 8px 1px",
                                                     background: "transparent",
                                                     paddingLeft: 15,
                                                     borderRadius: "0px"
@@ -796,7 +796,7 @@
                                                 display: "flex",
                                                 flexDirection: "column",
                                                 style: {
-                                                    boxShadow: "#ffffff 0px 0px 8px 1px",
+                                                    boxShadow: "#487748 0px 0px 8px 1px",
                                                     backgroundColor: "transparent",
                                                     borderRadius: "0px",
                                                     marginTop: 10
@@ -867,7 +867,7 @@
                                                     children: Object(h.jsx)(P.a, {
                                                         style: (e = {
                                                             background: "transparent"
-                                                        }, Object(S.a)(e, "background", "#32CD32"), Object(S.a)(e, "boxShadow", "#ffffff 0px 0px 8px 1px"), Object(S.a)(e, "fontSize", "18px"), Object(S.a)(e, "borderRadius", "0px"), Object(S.a)(e, "width", "176px"), Object(S.a)(e, "height", "45px"), Object(S.a)(e, "color", "#ffffff"), Object(S.a)(e, "textTransform", "capitalize"), Object(S.a)(e, "fontWeight", "500"), Object(S.a)(e, "borderBottom", "5px solid #f8cb6e"), e),
+                                                        }, Object(S.a)(e, "background", "#32CD32"), Object(S.a)(e, "boxShadow", "#487748 0px 0px 8px 1px"), Object(S.a)(e, "fontSize", "18px"), Object(S.a)(e, "borderRadius", "0px"), Object(S.a)(e, "width", "176px"), Object(S.a)(e, "height", "45px"), Object(S.a)(e, "color", "#ffffff"), Object(S.a)(e, "textTransform", "capitalize"), Object(S.a)(e, "fontWeight", "500"), Object(S.a)(e, "borderBottom", "5px solid #f8cb6e"), e),
                                                         onClick: function() {
                                                             return V()
                                                         },
@@ -884,7 +884,7 @@
                                         children: Object(h.jsxs)(u.a, {
                                             width: "100%",
                                             height: "100%",
-                                            boxShadow: "#ffffff 0px 0px 10px 1px",
+                                            boxShadow: "#487748 0px 0px 10px 1px",
                                             style: {
                                                 background: "#90ee90"
                                             },
