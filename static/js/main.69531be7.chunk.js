f1bde684ef38d06c611aa975c24cf76f40fd2026
Xgrai <80608273+dxfi@users.noreply.github.com>
2023-10-24 18:12:18 +0700

    Update main.69531be7.chunk.js

--- static/js/main.69531be7.chunk.js
+++ static/js/main.69531be7.chunk.js
@@ -264,7 +264,7 @@
             }
             var N = "static/media/logo.png",
                 L = "static/media/logo.png",
-                E = "static/media/presale.7df692ae.gif",
+                E = "static/media/pepex.png",
                 D = "static/media/bnblogo.95c82668.svg",
                 q = n(642),
                 V = n(587),
